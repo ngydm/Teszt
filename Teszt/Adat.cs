3c73f573b99bcd84ec67b354cc4573ab928883f2
Ngydm VS <nagyadam20080427@gmail.com>
2026-01-30 08:32:55 +0100

    Adat osztály kész

---
+++ Teszt/Adat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt
+{
+	internal class Adat
+	{
+		private string nev;
+		private int ar;
+public Adat(string nev, int ar)
+		{
+			this.nev = nev;
+			this.ar = ar;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Ar { get => ar; set => ar = value; }
+	}
+}
